9b974c4b407b851744477023b3061cdb290b3074
Recordu <117724279+Recordu@users.noreply.github.com>
2023-03-30 12:54:02 +0530

    Update __main__.py

--- PikachuOpRobot/__main__.py
+++ PikachuOpRobot/__main__.py
@@ -39,8 +39,8 @@ from PikachuOpRobot.modules.helper_funcs.alternate import typing_action
 
 
 PM_START_TEXT = f"""
-`𝐇𝐢..𝐖𝐞𝐥𝐜𝐨𝐦𝐞` [😎](https://telegra.ph/file/aebae6e955f9ba4adcedf.jpg)
-`𝐈'𝐦` 𝐏𝐢𝐤𝐚𝐜𝐡𝐮 🤗
+`𝐇𝐢..𝐖𝐞𝐥𝐜𝐨𝐦𝐞` [😎](https://telegra.ph/file/75a0dee096c4570c57135.jpg)
+`𝐈'𝐦` 𝐋𝐮𝐜𝐢𝐟𝐞𝐫 🤗
 `𝐈'𝐦 𝐇𝐞𝐫𝐞 𝐓𝐨 𝐇𝐞𝐥𝐩 𝐘𝐨𝐮 𝐌𝐚𝐧𝐚𝐠𝐞 𝐘𝐨𝐮𝐫 𝐆𝐫𝐨𝐮𝐩𝐬.. 𝐂𝐥𝐢𝐜𝐤 𝐇𝐞𝐥𝐩 𝐁𝐮𝐭𝐭𝐨𝐧 𝐓𝐨 𝐅𝐢𝐧𝐝 𝐎𝐮𝐭 𝐌𝐨𝐫𝐞 𝐀𝐛𝐨𝐮𝐭 𝐇𝐨𝐰 𝐓𝐨 𝐔𝐬𝐞 𝐌𝐞 𝐓𝐨 𝐌𝐲 𝐅𝐮𝐥𝐥 𝐏𝐨𝐭𝐞𝐧𝐭𝐢𝐚𝐥..`
 """
 
@@ -53,9 +53,9 @@ buttons += [[InlineKeyboardButton(text="👾 𝐀𝐝𝐝 𝐌𝐞 𝐓𝐨 𝐘
 ]]
 
 buttons += [[InlineKeyboardButton(text="🎉 𝐉𝐨𝐢𝐧 𝐅𝐨𝐫 𝐔𝐩𝐝𝐚𝐭𝐞𝐬 🎉",
-                                  url="https://t.me/TG_CODERS_UPDATES"),
+                                  url="https://t.me/Avengersbotz"),
              InlineKeyboardButton(text="💝 𝐒𝐮𝐩𝐩𝐨𝐫𝐭 𝐆𝐫𝐨𝐮𝐩 💝",
-                                  url="https://t.me/TG_X_CODERS"),
+                                  url="https://t.me/avengers_support_group"),
 ]]
 
 buttons += [[InlineKeyboardButton(text="💫 𝐂𝐥𝐨𝐬𝐞 𝐌𝐚𝐢𝐧 𝐌𝐞𝐧𝐮 💫",
@@ -63,7 +63,7 @@ buttons += [[InlineKeyboardButton(text="💫 𝐂𝐥𝐨𝐬𝐞 𝐌𝐚𝐢�
 
 
 HELP_STRINGS = f"""
-`𝐇𝐢.. 𝐈'𝐦` 𝐏𝐢𝐤𝐚𝐜𝐡𝐮 [😎](https://telegra.ph/file/8d7f24cd83fd817d3405f.jpg)
+`𝐇𝐢.. 𝐈'𝐦` 𝐋𝐮𝐜𝐢𝐟𝐞𝐫 [😎](https://telegra.ph/file/75a0dee096c4570c57135.jpg)
 `𝐂𝐥𝐢𝐜𝐤 𝐨𝐧 𝐭𝐡𝐞 𝐛𝐮𝐭𝐭𝐨𝐧𝐬 𝐛𝐞𝐥𝐨𝐰 𝐭𝐨 𝐠𝐞𝐭 𝐝𝐨𝐜𝐮𝐦𝐞𝐧𝐭𝐚𝐭𝐢𝐨𝐧 𝐚𝐛𝐨𝐮𝐭 𝐬𝐩𝐞𝐜𝐢𝐟𝐢𝐜 𝐦𝐨𝐝𝐮𝐥𝐞𝐬..`"""
 
 IMPORTED = {}
@@ -176,7 +176,7 @@ def start(update, context):
                 disable_web_page_preview=False,
             )
     else:
-        update.effective_message.reply_text("𝐇𝐞𝐲𝐚...,𝐏𝐢𝐤𝐚𝐜𝐡𝐮 🔥 𝐇𝐞𝐫𝐞,𝐇𝐨𝐰 𝐂𝐚𝐧 𝐈 𝐇𝐞𝐥𝐩 𝐘𝐨𝐮? 🧐")
+        update.effective_message.reply_text("𝐇𝐞𝐲𝐚.... 𝐋𝐮𝐜𝐢𝐟𝐞𝐫 🔥 𝐇𝐞𝐫𝐞,𝐇𝐨𝐰 𝐂𝐚𝐧 𝐈 𝐇𝐞𝐥𝐩 𝐘𝐨𝐮? 🧐")
 
 
 
@@ -641,7 +641,7 @@ def main():
         updater.start_polling(timeout=15, read_latency=4)
         updater.bot.send_message(
             chat_id=MESSAGE_DUMP,
-            text="𝐏𝐢𝐤𝐚 𝐏𝐢𝐤𝐚 𝐈 𝐍𝐨𝐰 𝐎𝐧𝐥𝐢𝐧𝐞")
+            text="𝐋𝐮𝐜𝐢𝐟𝐞𝐫 𝐈 𝐍𝐨𝐰 𝐎𝐧𝐥𝐢𝐧𝐞")
         client.run_until_disconnected()
 
     updater.idle()
